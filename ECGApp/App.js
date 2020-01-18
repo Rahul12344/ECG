07cf19d18abb636d0ea44b552991fd9037a78a7a
Amy Sywe <amysywe@wifi-131-179-31-202.host.ucla.edu>
2020-01-18 10:31:15 -0800

    added graph

--- ECGApp/App.js
+++ ECGApp/App.js
@@ -1,32 +1,19 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { SafeAreaView, View, Text, StatusBar } from 'react-native';
 
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+
 import GraphScreen from './components/GraphScreen.js';
 import HealthScreen from './components/HealthScreen.js';
 import SettingScreen from './components/SettingScreen.js';
-/*
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        
-      </SafeAreaView>
-    </>
-  );
-};*/
 
-const App = createBottomTabNavigator({
-  Graph: { screen: GraphScreen },
-  Health: { screen: HealthScreen },
-  Settings: { screen: SettingScreen },
-});
 
-export default createAppContainer(App);
+const TabNavigator = createBottomTabNavigator({
+    Graph: { screen: GraphScreen },
+    Health: { screen: HealthScreen },
+    Settings: { screen: SettingScreen },
+  },
+);
+
+export default createAppContainer(TabNavigator);
